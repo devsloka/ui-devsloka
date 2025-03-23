32c8f7a34b65f9cba0450c31191305b9543037cb
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-03-23 23:40:48 +0530

    Create sidebarItems.ts

---
+++ src/utilities/sidebarItems.ts
@@ -0,0 +1,65 @@
+export const sidebarItems = [
+  {
+    title: "Background",
+    children: [
+      {
+        title: "Colors",
+        href: "/components",
+      },
+      {
+        title: "Gradients",
+        href: "/background/gradients",
+      },
+      {
+        title: "Patterns",
+        href: "/background/patterns",
+      },
+    ],
+  },
+  {
+    title: "Components",
+    children: [
+      {
+        title: "Buttons",
+        href: "/components/buttons",
+      },
+      {
+        title: "Cards",
+        href: "/components/cards",
+      },
+      {
+        title: "Forms",
+        href: "/components/forms",
+      },
+    ],
+  },
+  {
+    title: "Utilities",
+    children: [
+      {
+        title: "Spacing",
+        href: "/utilities/spacing",
+      },
+      {
+        title: "Typography",
+        href: "/utilities/typography",
+      },
+      {
+        title: "Flexbox",
+        href: "/utilities/flexbox",
+      },
+      {
+        title: "Spacing",
+        href: "/utilities/spacing",
+      },
+      {
+        title: "Typography",
+        href: "/utilities/typography",
+      },
+      {
+        title: "Flexbox",
+        href: "/utilities/flexbox",
+      },
+    ],
+  },
+];
