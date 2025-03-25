3da64edc4964c23656b35d98875b0a347decef54
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-03-25 17:25:55 +0530

    Update page.tsx

--- src/app/(components)/components/expanding-cards/page.tsx
+++ src/app/(components)/components/expanding-cards/page.tsx
@@ -1,12 +1,18 @@
+import React from "react";
+import fs from "fs";
+import path from "path";
 import { ExpandingCards } from "@/components/devsloka-components/ExpandingCards";
 import { AdvancedCodeBlock } from "@/components/ui/advanced-code-block";
-import React from "react";
 
 const ExpandingCardsPage = () => {
+  const componentPath = path.resolve(
+    "src/components/devsloka-components/ExpandingCards.tsx"
+  );
+  const ExpandingCardsCode = fs.readFileSync(componentPath, "utf-8");
   return (
     <div>
       <AdvancedCodeBlock
-        code={`asd`}
+        code={ExpandingCardsCode}
         preview={<ExpandingCards />}
         language="tsx"
         showLineNumbers
