948715f669cf16c22d8c1d273b0886416841c1ea
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-03-26 14:07:48 +0530

    Update comp-sidebar.tsx

--- src/components/comp-layout/comp-sidebar.tsx
+++ src/components/comp-layout/comp-sidebar.tsx
@@ -5,15 +5,22 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 import { sidebarItems } from "@/utilities/sidebarItems";
+import { motion } from "motion/react";
 
-const CompSiidebar = () => {
+const CompSidebar = () => {
   const pathname = usePathname();
 
   return (
-    <aside className="hidden lg:flex fixed top-12 bottom-0 left-0 w-64 overflow-y-auto border-r p-4 rounded-r-2xl bg-background">
-      <nav className="w-full space-y-6">
-        {sidebarItems.map((section) => (
-          <div key={section.title} className="space-y-1">
+    <motion.aside className="hidden lg:flex fixed top-12 bottom-0 w-64 overflow-y-auto border-x border-dashed p-4 bg-background">
+      <motion.nav className="w-full space-y-6">
+        {sidebarItems.map((section, index) => (
+          <motion.div
+            key={section.title}
+            className="space-y-1"
+            initial={{ x: -20, opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ delay: 0.1 + index * 0.05, duration: 0.3 }}
+          >
             <h3 className="px-4 text-sm font-bold">{section.title}</h3>
             <div className="flex flex-col space-y-1">
               {section.children.map((item) => (
@@ -31,35 +38,14 @@ const CompSiidebar = () => {
                   >
                     <span className="truncate text-xs">{item.title}</span>
                   </Link>
-
-                  {/* Nested children links */}
-                  {/* {item.children && (
-                    <div className="ml-4 space-y-1">
-                      {item.children.map((subItem) => (
-                        <Link
-                          key={subItem.href}
-                          href={subItem.href}
-                          className={cn(
-                            "group flex items-center px-4 py-2 rounded-lg",
-                            "text-muted-foreground hover:text-foreground",
-                            "text-sm transition-colors duration-200",
-                            pathname === subItem.href &&
-                              "text-blue-600 bg-blue-500/10 font-medium border-l-4 border-blue-500"
-                          )}
-                        >
-                          <span className="truncate">{subItem.title}</span>
-                        </Link>
-                      ))}
-                    </div>
-                  )} */}
                 </div>
               ))}
             </div>
-          </div>
+          </motion.div>
         ))}
-      </nav>
-    </aside>
+      </motion.nav>
+    </motion.aside>
   );
 };
 
-export default CompSiidebar;
+export default CompSidebar;
