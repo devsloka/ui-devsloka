36d9e6c47374b27874fad05124a15a6ec45e5208
Yash Raj <112754137+yashraj970@users.noreply.github.com>
2025-03-27 18:50:04 +0530

    Cards page added

--- src/app/(components)/components/cards/[name]/page.tsx
+++ src/app/(components)/components/cards/[name]/page.tsx
@@ -1,13 +1,10 @@
 import { notFound } from "next/navigation";
 import { AdvancedCodeBlock } from "@/components/ui/advanced-code-block";
 import { getComponentCode } from "@/utilities/getComponentCode";
-import ParticleBackground from "@/components/backgrounds/particles-background";
-import BackgroundPreview from "@/components/backgrounds/preview/BackgroundPreview";
 import { Metadata } from "next";
 import ExploreAnimatedCard from "@/components/cards/explore-animated-card";
 
 const cards: Record<string, React.FC> = {
-  //   "particles-background": ParticleBackground,
   "explore-animated-card": ExploreAnimatedCard,
 };
 
@@ -24,18 +21,18 @@ export function generateMetadata({
 }): Metadata {
   const formattedName = params.name.replace(/-/g, " ");
   return {
-    title: `${formattedName} - Animated Background`,
-    description: `Explore the ${formattedName} animation, a beautiful background effect for web projects.`,
+    title: `${formattedName} - Animated Cards`,
+    description: `Explore the ${formattedName} animation, a beautiful cards effect for web projects.`,
     keywords: [
       formattedName,
-      "animated background",
-      "CSS background",
-      "React background effects",
+      "animated cards",
+      "CSS cards",
+      "React cards effects",
       "Next.js animated UI",
     ],
     openGraph: {
-      title: `${formattedName} - Animated Background`,
-      description: `Explore the ${formattedName} animation, a beautiful background effect for web projects.`,
+      title: `${formattedName} - Animated cards`,
+      description: `Explore the ${formattedName} animation, a beautiful cards effect for web projects.`,
       url: `https://yourwebsite.com/components/cards/${params.name}`,
       type: "website",
       images: [`https://yourwebsite.com/images/${params.name}.png`],
@@ -58,12 +55,7 @@ export default function CardPage({ params }: { params: { name: string } }) {
       <AdvancedCodeBlock
         code={componentCode}
         preview={
-          //   <BackgroundPreview
-          //     CardComponent={<CardComponent />}
-          //     title={params.name.replace(/-/g, " ")}
-          //     description="Beautiful animated cards for your website hero sections"
-          //   />
-          <div className="w-full max-w-7xl mx-auto">
+          <div className="w-full bg-background mx-auto">
             <CardComponent />
           </div>
         }
