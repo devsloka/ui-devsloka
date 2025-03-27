f48398664407f4743d216e50b793fa356360c79e
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-03-27 13:07:49 +0530

    Update Navbar.tsx

--- src/components/root/Navbar.tsx
+++ src/components/root/Navbar.tsx
@@ -39,7 +39,7 @@ const Navbar = () => {
   const pathname = usePathname();
   const sheetId = useId();
   return (
-    <nav className="fixed top-0 z-40 w-full border-b bg-background/50 backdrop-blur-xs h-16">
+    <nav className="fixed top-0 z-40 w-full border-b bg-background/80 backdrop-blur-lg h-16">
       <div className="h-full max-w-[88rem] mx-auto px-6 flex justify-between items-center">
         <div className="flex items-center gap-10">
           <div className="flex items-center gap-2">
