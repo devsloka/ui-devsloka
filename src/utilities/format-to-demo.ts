0127f45cd78f19d45aeab4ab74dac1c6adc4d4da
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-03-27 19:58:40 +0530

    Create format-to-demo.ts

---
+++ src/utilities/format-to-demo.ts
@@ -0,0 +1,10 @@
+export function formatToDemo(input: string): string {
+  return (
+    input
+      .split("-")
+      .map((word, index) =>
+        index === 0 ? word : word.charAt(0).toUpperCase() + word.slice(1)
+      )
+      .join("") + "Demo"
+  );
+}
