803a1421ae0b82fdc09e790b67350f4e7f30a769
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-04-03 23:47:51 +0530

    Update components.utils.ts

--- src/utilities/components.utils.ts
+++ src/utilities/components.utils.ts
@@ -4,6 +4,10 @@ import ShowcaseSlider from "@/components/devsloka-components/showcase-slider";
 import TourDemo from "@/components/devsloka-components/tour-demo";
 import FloatingDotsDemo from "@/components/devsloka-components/floating-dots-demo";
 import AirbnbListingCardExample from "@/components/devsloka-components/airbnb-card-demo";
+import MorphingNav from "@/components/devsloka-components/morphing-nav";
+import MorphingModal from "@/components/devsloka-components/morphing-modal";
+import ExpandableCard from "@/components/devsloka-components/morphing-card";
+import SuccessResult from "@/components/devsloka-components/success-result";
 
 export type ComponentMeta = {
   component: React.FC;
@@ -84,6 +88,50 @@ export const components: Record<string, ComponentMeta> = {
     component: AirbnbListingCardExample,
     codeMetadata: {
       title: "Airbnb Card",
+      description:
+        "Interactive tour component to guide users through your application",
+      keywords: ["React", "Tour", "Guide", "User Experience"],
+      language: "tsx",
+      dependencies: "npm i motion",
+    },
+  },
+  "morphing-nav": {
+    component: MorphingNav,
+    codeMetadata: {
+      title: "Morphing Nav",
+      description:
+        "Interactive tour component to guide users through your application",
+      keywords: ["React", "Tour", "Guide", "User Experience"],
+      language: "tsx",
+      dependencies: "npm i motion",
+    },
+  },
+  "morphing-modal": {
+    component: MorphingModal,
+    codeMetadata: {
+      title: "Morphing Modal",
+      description:
+        "Interactive tour component to guide users through your application",
+      keywords: ["React", "Tour", "Guide", "User Experience"],
+      language: "tsx",
+      dependencies: "npm i motion",
+    },
+  },
+  "morphing-card": {
+    component: ExpandableCard,
+    codeMetadata: {
+      title: "Expandable Card",
+      description:
+        "Interactive tour component to guide users through your application",
+      keywords: ["React", "Tour", "Guide", "User Experience"],
+      language: "tsx",
+      dependencies: "npm i motion",
+    },
+  },
+  "success-result": {
+    component: SuccessResult,
+    codeMetadata: {
+      title: "Success Result",
       description:
         "Interactive tour component to guide users through your application",
       keywords: ["React", "Tour", "Guide", "User Experience"],
