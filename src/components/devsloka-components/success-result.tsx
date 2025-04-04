d59d977c7bf3d76f801f723091f442e6b30d70bb
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-04-04 13:10:18 +0530

    Update success-result.tsx

--- src/components/devsloka-components/success-result.tsx
+++ src/components/devsloka-components/success-result.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { motion } from "framer-motion";
+import { AnimatePresence, motion } from "motion/react";
 import { DotLottieReact } from "@lottiefiles/dotlottie-react";
 import { Card, CardContent } from "../ui/card";
 
@@ -13,39 +13,41 @@ const SuccessResult: React.FC = () => {
   };
 
   return (
-    <Card className="max-w-sm mx-auto shadow-lg">
-      <CardContent className="flex flex-col items-center p-6">
-        <div className="w-32 h-32">
-          <DotLottieReact src="/dot-lottie/success.lottie" loop autoplay />
-        </div>
+    <AnimatePresence>
+      <Card className="max-w-sm mx-auto shadow-lg">
+        <CardContent className="flex flex-col items-center p-6">
+          <div className="w-32 h-32">
+            <DotLottieReact src="/dot-lottie/success.lottie" loop autoplay />
+          </div>
 
-        {/* Animated Heading */}
-        <motion.h2
-          initial="initial"
-          animate="animate"
-          variants={textVariants}
-          transition={{ duration: 0.6, ease: "easeOut" }}
-          className="mt-4 text-xl font-semibold text-center text-green-600"
-        >
-          Success!
-        </motion.h2>
+          {/* Animated Heading */}
+          <motion.h2
+            initial="initial"
+            animate="animate"
+            variants={textVariants}
+            transition={{ duration: 0.6, ease: "easeOut" }}
+            className="mt-4 text-xl font-semibold text-center text-blue-600"
+          >
+            Success!
+          </motion.h2>
 
-        {/* Animated Description */}
-        <motion.p
-          initial="initial"
-          animate="animate"
-          variants={textVariants}
-          transition={{
-            duration: 0.8,
-            ease: "easeOut",
-            delay: 0.2, // Staggered animation
-          }}
-          className="mt-2 text-center text-gray-600"
-        >
-          Your operation was completed successfully.
-        </motion.p>
-      </CardContent>
-    </Card>
+          {/* Animated Description */}
+          <motion.p
+            initial="initial"
+            animate="animate"
+            variants={textVariants}
+            transition={{
+              duration: 0.8,
+              ease: "easeOut",
+              delay: 0.2, // Staggered animation
+            }}
+            className="mt-2 text-center text-gray-600"
+          >
+            Your operation was completed successfully.
+          </motion.p>
+        </CardContent>
+      </Card>
+    </AnimatePresence>
   );
 };
 
