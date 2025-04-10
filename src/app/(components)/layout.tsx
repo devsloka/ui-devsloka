0d388d5c94f81c704cd263de0c138d1a9f09014f
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-04-11 02:45:36 +0530

    ± layout

--- src/app/(components)/layout.tsx
+++ src/app/(components)/layout.tsx
@@ -7,14 +7,14 @@ export default function ComponentLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="container-wrapper">
+    <div className="container-wrapper lg:border-r border-dashed pr-2">
       <div className="container flex-1 items-start md:grid md:grid-cols-[0px_minmax(0,1fr)] md:gap-6 lg:grid-cols-[260px_minmax(0,1fr)] lg:gap-10">
-        <aside className="hidden lg:flex top-14 z-30 h-[calc(100vh-3.5rem)] w-full border-r md:sticky md:block">
+        <aside className="hidden lg:flex top-14 z-30 h-[calc(100vh-3.5rem)] w-full border-r border-dashed md:sticky md:block">
           <div className="no-scrollbar h-full overflow-y-auto overflow-x-hidden">
             <CompSidebar />
           </div>
         </aside>
-        <div className="mt-20">
+        <div className="mt-20 min-h-screen">
           <DynamicBreadcrumb />
           {children}
         </div>
