d4332cb951d2b70ffe64fea76e903e954512572d
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-04-12 21:51:36 +0530

    Create gradient-text-demo.tsx

---
+++ src/components/devsloka-components/demo/gradient-text-demo.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import GradientText from "../gradient-text";
+
+const GradientTextDemo = () => {
+  return (
+    <>
+      <GradientText
+        className="text-xl lg:text-4xl font-bold text-center"
+        animationDuration={2}
+      >
+        Welcome to Devsloka Ui
+      </GradientText>
+    </>
+  );
+};
+
+export default GradientTextDemo;
