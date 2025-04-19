1e65ac5868a1aac5a855419d6dae8ea1b55c1d98
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-04-19 20:56:32 +0530

    Create card-decorator-demo.tsx

---
+++ src/components/devsloka-components/demo/card-decorator-demo.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { CardDecorator } from "../card-decorator";
+import { cn } from "@/lib/utils";
+
+const CardDecoratorDemo: React.FC = () => {
+  return (
+    <div
+      className={cn(
+        "group relative shadow-zinc-950/5 h-40 w-64 border border-dashed flex justify-center items-center"
+      )}
+    >
+      <CardDecorator variant={"default"} />
+      <div className={cn("p-4")}>
+        <h2 className={cn("text-lg font-semibold")}>Card Title</h2>
+        <p className={cn("text-sm text-muted-foreground")}>Card description</p>
+      </div>
+    </div>
+  );
+};
+
+export default CardDecoratorDemo;
