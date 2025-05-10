8bca65f7b9ef0b5c01885f5422a9d9515f3bf250
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-05-10 18:46:12 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["images.unsplash.com", "plus.unsplash.com", "images.pexels.com", 'avatars.githubusercontent.com'],
+    domains: [
+      "images.unsplash.com",
+      "plus.unsplash.com",
+      "images.pexels.com",
+      "avatars.githubusercontent.com",
+      "source.unsplash.com",
+    ],
   },
 };
 
