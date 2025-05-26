be31fac5b8f3086cec5949eabcc2f430ded345a2
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2025-05-27 01:16:23 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,23 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)", // Apply to all routes
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN", // or 'DENY' if you never want embedding
+          },
+          {
+            key: "Content-Security-Policy",
+            value: "frame-ancestors 'self';", // Modern protection
+          },
+        ],
+      },
+    ];
+  },
   images: {
     domains: [
       "images.unsplash.com",
